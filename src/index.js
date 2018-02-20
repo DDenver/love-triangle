508e9eba00a108f499675bbc907ae4cd165963a7
Denis <den107620@gmail.com>
2018-02-20 23:11:45 +0300

    Love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,40 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  arr = preferences;
+  arrLove = [];
+  
+  //Оставляет уникальные элементы массива
+  function unique(arr) {
+      var result = [];
+    
+      nextInput:
+        for (var i = 0; i < arr.length; i++) {
+          var str = arr[i]; // для каждого элемента
+          for (var j = 0; j < result.length; j++) { // ищем, был ли он уже?
+            if (result[j] == str) continue nextInput; // если да, то следующий
+          }
+          result.push(str);
+        }
+    
+      return result;
+    }
+  
+  for (i = 0; i < (arr.length); i++){
+    firstLover = arr[i];
+    secondLover = arr[firstLover-1];
+    thidLover = arr[secondLover-1];
+    if (thidLover == i+1){
+      arrLove.push(firstLover);
+      arrLove.push(secondLover);
+      arrLove.push(thidLover);
+      console.log(arrLove);
+    };
+  };
+  
+  arrLove = unique(arrLove);
+  
+  o = Math.floor(arrLove.length / 3);
+  return o;
+  
 };
